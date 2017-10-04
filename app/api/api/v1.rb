2102e37c5a3b01c0ba6a0a6552fe13b8feb34f93
Abraham Barrera <abarrerac@gmail.com>
2017-10-04 20:42:18 -0300

    get one account

--- app/api/api/v1.rb
+++ app/api/api/v1.rb
@@ -9,6 +9,24 @@ module Api
       get do
         Account.all
       end
+
+      get '/:id' do
+        Account.find_by(id: params[:id])
+      end
+
+      route_param :id do
+        desc 'get one account'
+        get do
+          Account.find_by(id: params[:id])
+        end
+
+        # desc 'update '
+        # put do
+        #   account = Account.find_by(id: params[:id])
+        #   account.update params
+        #   account
+        # end
+      end
     end
   end
 end
